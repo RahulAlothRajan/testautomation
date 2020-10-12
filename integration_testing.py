ba513fde60ef21c43718af378567381a7d39e1c7
Rahul <Rahul Aloth Rajan>
2020-10-12 10:30:43 +0200

    First commit

---
+++ integration_testing.py
@@ -0,0 +1,18 @@
+import pytest
+
+def deploy_proai_package():
+    
+    pass  # perform some webtest test for your app
+
+
+def test_something_quick():
+    pass
+
+
+def test_another():
+    pass
+
+
+class TestClass:
+    def test_method(self):
+        pass
