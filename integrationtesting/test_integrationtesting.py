6971dc3af04cbe11c9214f5111c8ef96769c5df4
Rahul <Rahul Aloth Rajan>
2020-10-13 00:25:19 +0200

    giving thumps up

--- integrationtesting/test_integrationtesting.py
+++ integrationtesting/test_integrationtesting.py
@@ -1,14 +1,27 @@
 """sample test"""
 import unittest
+from termcolor import colored
+import emoji
 
 from integrationtesting import integration_testing
 
+def print_error(string:str):
+    text = colored(string, 'red')
+    print(text + emoji.emojize(':thumbs_down:'))
+    pass
+def print_warning(string:str):
+    text = colored(string, 'yellow')
+    pass
+def print_success(string:str):
+    text = colored(string, 'yellow')
+    pass
 
 class Testintegration_testing(unittest.TestCase):
     """sample test"""
 
     def test_world(self):
         """sample test"""
+        print_error("What the hell")
         self.assertEqual(1, 1)
 
     def test_world_unicode(self):
