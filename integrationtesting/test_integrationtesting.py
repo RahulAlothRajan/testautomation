7608d44c2a5115f953141c9fe58f176ed3371482
Rahul <Rahul Aloth Rajan>
2020-10-13 22:56:54 +0200

    space

--- integrationtesting/test_integrationtesting.py
+++ integrationtesting/test_integrationtesting.py
@@ -18,13 +18,13 @@ class Testintegration_testing(unittest.TestCase):
 
     def test_world_unicode1(self):
         """sample test with unicode"""
-        plt.scatter(x, y, s=area, c=colors, alpha=0.5)
         # Create data
         N = 500
         x = np.random.rand(N)
         y = np.random.rand(N)
         colors = (0,0,0)
         area = np.pi*3
+        plt.scatter(x, y, s=area, c=colors, alpha=0.5)
         plt.title('Scatter plot pythonspot.com')
         plt.xlabel('x')
         plt.ylabel('y')
